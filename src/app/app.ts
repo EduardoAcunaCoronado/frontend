6c51da4594669473ac18e61980ecdde1bd233610
eduardoacunacoronado <edu191191@gmail.com>
2025-10-29 14:02:56 +0100

    Agregar campos al formulario.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FormFirst} from './form-first/form-first';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormFirst],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/form-first/form-first.ts
+++ src/app/form-first/form-first.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-form-first',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './form-first.html',
   styleUrl: './form-first.css',
 })
 export class FormFirst {
+  status = "Form not submitted";
+  defaultName = "";
+  user = "";
 
+  onSendForm(email: HTMLInputElement){
+    console.log(email.value);
+    this.status = "Form submitted";
+  }
+
+  onDefaultForm() {
+    this.defaultName = "Default Name";
+  }
 }
